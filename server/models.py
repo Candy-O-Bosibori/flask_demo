26daf5d40e74ea8018aaa64d95021ff6ace4a152
Candy Bosibori Ongaro <candybosibori04@gmail.com>
2024-04-11 01:27:50 +0300

    done

--- server/models.py
+++ server/models.py
@@ -17,51 +17,7 @@ metadata = MetaData(naming_convention=convention)
 
 db = SQLAlchemy(metadata=metadata)
 
-
-# employee_meetings = db.Table(
-#     'employees_meetings',
-#     metadata,
-#     db.Column('employee_id', db.Integer, db.ForeignKey(
-#         'employees.id'), primary_key=True),
-#     db.Column('meeting_id', db.Integer, db.ForeignKey(
-#         'meetings.id'), primary_key=True)
-# )
-
-# class Employee(db.Model):
-#     __tablename__ = 'employees'
-
-#     id = db.Column(db.Integer, primary_key=True)
-#     name = db.Column(db.String)
-#     hire_date = db.Column(db.Date)
-#     meetings = db.relationship(
-#         'Meeting', secondary=employee_meetings, back_populates='employees')
-    
-#     assignments = db.relationship(
-#         'Assignment', back_populates='employee', cascade='all, delete-orphan')
-    
-#     projects = association_proxy('assignments', 'project',
-#                                  creator=lambda project_obj: Assignment(project=project_obj) )
-    
-#     def __repr__(self):
-#         return f'<Employee {self.id}, {self.name}, {self.hire_date}>'
-
-
-# class Meeting(db.Model):
-#     __tablename__ = 'meetings'
-
-#     id = db.Column(db.Integer, primary_key=True)
-#     topic = db.Column(db.String)
-#     scheduled_time = db.Column(db.DateTime)
-#     location = db.Column(db.String)
-#     employees = db.relationship(
-#         'Employee', secondary=employee_meetings, back_populates='meetings')
-    
-    
-
-#     def __repr__(self):
-#         return f'<Meeting {self.id}, {self.topic}, {self.scheduled_time}, {self.location}>'
-
-
+ 
 
 class User(db.Model, SerializerMixin):
     __tablename__='users'
